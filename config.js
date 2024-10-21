94c23b601f2ed223567b8c620c239260becacb2a
PedroFilipe08 <pedro.schmittel@escola.pr.gov.br>
2024-10-21 15:44:32 -0300

    Update and rename configuraçoes.html to config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+function toggleConfigPanel() {
+    const configPanel = document.getElementById('config-panel');
+    configPanel.style.display = configPanel.style.display === 'none' || configPanel.style.display === '' ? 'block' : 'none';
+}
